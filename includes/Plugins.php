02cc09d7aa54732e9ff66cbe73490cc724fc3751
Florestan Bredow <daiko@daiko.fr>
2017-01-11 16:41:46 +0100

    Documente et nettoie la classe Plugins

--- includes/Plugins.php
+++ includes/Plugins.php
@@ -1,46 +1,21 @@
 <?php
-
-# ***** BEGIN LICENSE BLOCK *****
-# This file is part of DotClear.
-# Copyright (c) 2004 Olivier Meunier and contributors. All rights
-# reserved.
-#
-# DotClear is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# DotClear is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with DotClear; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-# ***** END LICENSE BLOCK *****
-
-/*
-Classe de gestion des plugins et des thémes
-*/
-
 class Plugins
 {
     public $location;
-    public $type;
-    public $xml;
     public $pluginsList = array();
 
-    public function __construct($location, $type = 'plugin')
+    public function __construct($location)
     {
         $this->location = null;
         if (is_dir($location)) {
             $this->location = $location.'/';
         }
-        $this->type = $type;
     }
 
+    /**
+     * Get list of all plugins in extension directory
+     * @return array|bool Plugin list on success, false on error.
+     */
     public function getAllPlugins()
     {
         if (($listFiles = $this->readDir()) !== false) {
@@ -56,23 +31,10 @@ class Plugins
         return false;
     }
 
-    public function loadTraductions($lang = 'fr')
-    {
-        foreach ($this->pluginsList as $pluginName => $v) {
-            // language files : first default language, then preferred language
-            $langPath = $this->location . $pluginName . '/' . 'lang/' . $pluginName;
-
-            if (file_exists($langPath . '_fr.inc.php')) {
-                include($langPath . '_fr.inc.php');
-            }
-
-            if ($lang != 'fr'
-                and file_exists($langPath . '_' . $lang . '.inc.php')) {
-                include($langPath . '_' . $lang . '.inc.php');
-            }
-        }
-    }
-
+    /**
+     * Get list of all active plugins in extension directory.
+     * @return array|bool Plugin list on success, false on error.
+     */
     public function getActivePlugins()
     {
         if (($listFiles = $this->readDir()) !== false) {
@@ -90,29 +52,35 @@ class Plugins
         return false;
     }
 
-    /* Lecture d'un répertoire é la recherche des desc.xml */
-    private function readDir()
+    /**
+     * Load traduction for plugins.
+     * @param  string $lang [description]
+     * @return [type]       [description]
+     */
+    public function loadTraductions($lang = 'fr')
     {
-        if ($this->location === null) {
-            return false;
-        }
+        $pluginsNameList = array_keys($this->pluginsList);
+        foreach ($pluginsNameList as $pluginName) {
+            // language files : first default language, then preferred language
+            $langPath = $this->location . $pluginName . '/' . 'lang/' . $pluginName;
 
-        $res = array();
-        $dir = dir($this->location);
-        # Liste du répertoire des plugins
-        while (($entry = $dir->read()) !== false) {
-            if ($entry != '.'
-                and $entry != '..'
-                and is_dir($this->location.$entry)
-                and file_exists($this->location.$entry . '/desc.xml')
-            ) {
-                $res[$entry] = $this->location.$entry . '/desc.xml';
+            if (file_exists($langPath . '_fr.inc.php')) {
+                include($langPath . '_fr.inc.php');
+            }
+
+            if ($lang != 'fr'
+                and file_exists($langPath . '_' . $lang . '.inc.php')) {
+                include($langPath . '_' . $lang . '.inc.php');
             }
         }
-        return $res;
     }
 
-    private function getPluginInfo($plugin)
+    /**
+     * Read desc.xml plugin's file
+     * @param  string $plugin   path to plugin's desc.xml file.
+     * @return array|bool       Information on success, false on error
+     */
+    public function getPluginInfo($plugin)
     {
         if (!file_exists($plugin)) {
             return false;
@@ -134,5 +102,30 @@ class Plugins
         }
 
         return $infos;
+    }
+
+    /**
+     * List all desc.xml file in sub directory of /tools directory.
+     * @return array|bool list of desc.xml file on success, false on error.
+     */
+    private function readDir()
+    {
+        if ($this->location === null) {
+            return false;
+        }
+
+        $res = array();
+        $dir = dir($this->location);
+        # Liste du répertoire des plugins
+        while (($entry = $dir->read()) !== false) {
+            if ($entry != '.'
+                and $entry != '..'
+                and is_dir($this->location.$entry)
+                and file_exists($this->location.$entry . '/desc.xml')
+            ) {
+                $res[$entry] = $this->location.$entry . '/desc.xml';
+            }
+        }
+        return $res;
     }
 }
