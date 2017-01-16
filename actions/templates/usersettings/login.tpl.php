04537965c096b8aabd3ecf7db6964a42e890ad12
Florestan Bredow <daiko@daiko.fr>
2017-01-16 13:27:08 +0100

    Sort le html du code de l'action usersetting

---
+++ actions/templates/usersettings/login.tpl.php
@@ -0,0 +1,45 @@
+<input type="hidden" name="action" value="login" />
+<table>
+    <tr>
+        <td></td>
+        <td><?php echo _t('IF_YOU_ARE_REGISTERED_LOGGIN_HERE'); ?></td>
+    </tr>
+    <?php
+    if (isset($error))
+    {
+        echo "<tr><td></td><td><div class=\"alert alert-danger\">", $error, "</div></td></tr>\n";
+    }
+    ?>
+    <tr>
+        <td align="right"><?php echo _t('YOUR_WIKINAME'); ?>&nbsp;:</td>
+        <td><input name="name" size="40" value="<?php if (isset($name)) echo htmlspecialchars($name, ENT_COMPAT, YW_CHARSET) ?>" /></td>
+    </tr>
+    <tr>
+        <td align="right"><?php echo _t('PASSWORD_5_CHARS_MINIMUM'); ?>&nbsp;:</td>
+        <td>
+        <input type="password" name="password" size="40" />
+        <input type="hidden" name="remember" value="0" />
+        <input type="checkbox" name="remember" value="1" />&nbsp;<?php echo _t('REMEMBER_ME'); ?>.
+        </td>
+    </tr>
+    <tr>
+        <td></td>
+        <td><input type="submit" value="<?php echo _t('IDENTIFICATION'); ?>" size="40" /></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td><?php echo _t('FILL_THE_NEXT_FIELDS_IF_YOU_LOGGIN_FOR_THE_FIRST_TIME_AND_REGISTER'); ?></td>
+    </tr>
+    <tr>
+        <td align="right"><?php echo _t('PASSWORD_CONFIRMATION'); ?>&nbsp;:</td>
+        <td><input type="password" name="confpassword" size="40" /></td>
+    </tr>
+    <tr>
+        <td align="right"><?php echo _t('YOUR_EMAIL_ADDRESS'); ?>.&nbsp;:</td>
+        <td><input name="email" size="40" value="<?php if (isset($email)) echo htmlspecialchars($email, ENT_COMPAT, YW_CHARSET) ?>" /></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td><input type="submit" value="<?php echo _t('NEW_ACCOUNT'); ?>" size="40" /></td>
+    </tr>
+</table>
--- actions/usersettings.php
+++ actions/usersettings.php
@@ -75,78 +75,11 @@ else if ($user = $this->getUser())
     }
     // user is logged in; display config form
     echo $this->formOpen();
-    ?>
-    <input type="hidden" name="action" value="update" />
-    <table>
-        <tr>
-            <td align="right"></td>
-            <td><?php echo _t('GREETINGS'); ?>, <?php echo $this->link($user["name"]) ?>&nbsp;!</td>
-        </tr>
-        <tr>
-            <td align="right"><?php echo _t('YOUR_EMAIL_ADDRESS'); ?>&nbsp;:</td>
-            <td><input name="email" value="<?php echo htmlspecialchars($user["email"], ENT_COMPAT, YW_CHARSET) ?>" size="40" /></td>
-        </tr>
-        <tr>
-            <td align="right"><?php echo _t('DOUBLE_CLICK_TO_EDIT'); ?>&nbsp;:</td>
-            <td><input type="hidden" name="doubleclickedit" value="N" /><input type="checkbox" name="doubleclickedit" value="Y" <?php echo $user["doubleclickedit"] == "Y" ? "checked=\"checked\"" : "" ?> /></td>
-        </tr>
-        <tr>
-            <td align="right"><?php echo _t('SHOW_COMMENTS_BY_DEFAULT'); ?>&nbsp;:</td>
-            <td><input type="hidden" name="show_comments" value="N" /><input type="checkbox" name="show_comments" value="Y" <?php echo $user["show_comments"] == "Y" ? "checked\"checked\"" : "" ?> /></td>
-        </tr>
-        <tr>
-            <td align="right"><?php echo _t('MAX_NUMBER_OF_LASTEST_COMMENTS'); ?>&nbsp;:</td>
-            <td><input name="changescount" value="<?php echo htmlspecialchars($user["changescount"], ENT_COMPAT, YW_CHARSET) ?>" size="40" /></td>
-        </tr>
-        <tr>
-            <td align="right"><?php echo _t('MAX_NUMBER_OF_VERSIONS'); ?>&nbsp;:</td>
-            <td><input name="revisioncount" value="<?php echo htmlspecialchars($user["revisioncount"], ENT_COMPAT, YW_CHARSET) ?>" size="40" /></td>
-        </tr>
-        <tr>
-            <td align="right"><?php echo _t('YOUR_MOTTO'); ?>&nbsp;:</td>
-            <td><input name="motto" value="<?php echo htmlspecialchars($user["motto"], ENT_COMPAT, YW_CHARSET) ?>" size="40" /></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td><input type="submit" value="<?php echo _t('UPDATE'); ?>" /> <input type="button" value="<?php echo _t('DISCONNECT'); ?>" onclick="document.location='<?php echo $this->href("", "", "action=logout"); ?>'" /></td>
-        </tr>
-    </table>
-
-    <?php
+    include('actions/templates/usersettings/update.tpl.html');
     echo $this->formClose();
 
     echo $this->formOpen();
-    ?>
-    <input type="hidden" name="action" value="changepass" />
-    <table>
-        <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-        </tr>
-        <tr>
-            <td align="right"></td>
-            <td><?php echo _t('CHANGE_THE_PASSWORD'); ?></td>
-        </tr>
-        <?php
-        if (isset($error))
-        {
-            echo "<tr><td></td><td><div class=\"alert alert-danger\">", $error, "</div></td></tr>\n";
-        }
-        ?>
-        <tr>
-            <td align="right"><?php echo _t('YOUR_OLD_PASSWORD'); ?>&nbsp;:</td>
-            <td><input type="password" name="oldpass" size="40" /></td>
-        </tr>
-        <tr>
-            <td align="right"><?php echo _t('NEW_PASSWORD'); ?>&nbsp;:</td>
-            <td><input type="password" name="password" size="40" /></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td><input type="submit" value="<?php echo _t('CHANGE'); ?>" size="40" /></td>
-        </tr>
-    </table>
-    <?php
+    include('actions/templates/usersettings/changepass.tpl.html');
     echo $this->formClose();
 
 }
@@ -208,52 +141,6 @@ else
     }
 
     echo $this->formOpen();
-    ?>
-    <input type="hidden" name="action" value="login" />
-    <table>
-        <tr>
-            <td></td>
-            <td><?php echo _t('IF_YOU_ARE_REGISTERED_LOGGIN_HERE'); ?></td>
-        </tr>
-        <?php
-        if (isset($error))
-        {
-            echo "<tr><td></td><td><div class=\"alert alert-danger\">", $error, "</div></td></tr>\n";
-        }
-        ?>
-        <tr>
-            <td align="right"><?php echo _t('YOUR_WIKINAME'); ?>&nbsp;:</td>
-            <td><input name="name" size="40" value="<?php if (isset($name)) echo htmlspecialchars($name, ENT_COMPAT, YW_CHARSET) ?>" /></td>
-        </tr>
-        <tr>
-            <td align="right"><?php echo _t('PASSWORD_5_CHARS_MINIMUM'); ?>&nbsp;:</td>
-            <td>
-            <input type="password" name="password" size="40" />
-            <input type="hidden" name="remember" value="0" />
-            <input type="checkbox" name="remember" value="1" />&nbsp;<?php echo _t('REMEMBER_ME'); ?>.
-            </td>
-        </tr>
-        <tr>
-            <td></td>
-            <td><input type="submit" value="<?php echo _t('IDENTIFICATION'); ?>" size="40" /></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td><?php echo _t('FILL_THE_NEXT_FIELDS_IF_YOU_LOGGIN_FOR_THE_FIRST_TIME_AND_REGISTER'); ?></td>
-        </tr>
-        <tr>
-            <td align="right"><?php echo _t('PASSWORD_CONFIRMATION'); ?>&nbsp;:</td>
-            <td><input type="password" name="confpassword" size="40" /></td>
-        </tr>
-        <tr>
-            <td align="right"><?php echo _t('YOUR_EMAIL_ADDRESS'); ?>.&nbsp;:</td>
-            <td><input name="email" size="40" value="<?php if (isset($email)) echo htmlspecialchars($email, ENT_COMPAT, YW_CHARSET) ?>" /></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td><input type="submit" value="<?php echo _t('NEW_ACCOUNT'); ?>" size="40" /></td>
-        </tr>
-    </table>
-    <?php
+    include('actions/templates/login.tpl.html');
     echo $this->formClose();
 }
